930bde4fe2d3952d87bec4b56b9528518223e450
sam <samt@samt.st>
2023-10-25 17:13:15 +0800

    fetch and display some extra basic user data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import React from "react"
 import * as NostrTools from "nostr-tools"
 import NDK, * as NDKUtil from "@nostr-dev-kit/ndk"
 
+type UserData = {
+	name: string | undefined
+	image: string | undefined
+	website: string | undefined
+}
+
 const App = () => {
 	const nip07signer = new NDKUtil.NDKNip07Signer()
 	const ndk = new NDK({
@@ -10,6 +16,8 @@ const App = () => {
 	})
 
 	const [following, setFollowing] = React.useState<NDKUtil.NDKTag[]>([])
+
+	const [userData, setUserData] = React.useState<Record<string, UserData>>({})
 
 	const connect = async () => {
 		// get the user from the browser extension - if we can
@@ -32,7 +40,36 @@ const App = () => {
 			console.log("tags", following)
 
 			setFollowing(following)
+
+			lookUpUserData()
 		}
+	}
+
+	const lookUpUserData = async () => {
+		console.log("user count", following.length)
+		let users: Record<string, UserData> = {}
+
+		for (let i = 0; i < following.length; i++) {
+			const hex = following[i][1]
+			console.log("hex " + i, hex)
+
+			const user = ndk.getUser({
+				hexpubkey: hex,
+				// npub: "npub1l2vyh47mk2p0qlsku7hg0vn29faehy9hy34ygaclpn66ukqp3afqutajft",
+			})
+			console.log("user " + i, user)
+			await user.fetchProfile()
+
+			const userData: UserData = {
+				name: user.profile?.name,
+				image: user.profile?.image,
+				website: user.profile?.website,
+			}
+			users[hex] = userData
+
+			console.log("user " + i + " profile:", user.profile)
+		}
+		setUserData(users)
 	}
 
 	React.useEffect(() => {
@@ -53,7 +90,8 @@ const App = () => {
 					const [p, hex, relay] = event
 					return (
 						<li key={key}>
-							{hex} {!!relay && <>{relay}</>}
+							{hex} {!!userData?.[hex]?.name && `(${userData[hex].name})`}{" "}
+							{!!relay && <>{relay}</>}
 						</li>
 					)
 				})}
